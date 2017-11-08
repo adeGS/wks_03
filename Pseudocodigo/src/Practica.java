d88641db01477bf440338dc93857a187dbf017e1
AdeGS <mssoperativesystem@gmail.com>
2017-11-08 21:14:36 +0100

    Ejecicios de pseudocódigo desde 33 a 42 (no hacer ej 39)

---
+++ Pseudocodigo/src/Practica.java
@@ -0,0 +1,21 @@
+/*Crear un array unidimensional de 20 elementos con nombres de personas. Visualizar los
+elementos de la lista debiendo ir cada uno en una fila distinta.*/
+public class Practica {
+
+	public static void main() {
+		// TODO Auto-generated method stub
+		String vecMuchosNombres[] = { };
+		String arrayNombre[]= new String [20];
+		
+		int indice;
+		indice = 0;
+		for( , f<20 , f++){
+			
+		}
+				
+		
+		
+
+	}//main			
+	
+}//class
--- T04Ej04/Tabla.java
+++ T04Ej04/Tabla.java
@@ -1,20 +1,26 @@
 import java.util.Scanner;
 public class Tabla {
 
-	public static void main(String[] args) {
+	public static void main() {
 		// TODO Auto-generated method stub
 
 		Scanner teclado  = new Scanner(System.in);
 		int letra;
-		String frase = new String ("letra");
+		String frase = new String ();
 		frase = teclado.next();
 		System.out.println("Introduzca una frase");
 			frase = teclado.next();
 		System.out.println("Escriba una palabra de la frase");
+		if (s1.startsWith(palabra+" ")){
+			System.out.println("Es la primera palabra de la frase, posicion " +s1.indexOf(palabra));
+		}else if (s1.indexOf(" " +palabra +" ")!=-1){
+			System.out.println("La palabra empieza en la posicion " + (s1.indexOf(" " + palabra +" ") +1));
+		}else if (s1.endsWith(" "+palabra)){
+			System.out.println("Es la ultima palabra de la frase, posicion " + (s1.lastIndexOf(" "+palabra) + 1));
+		} else{
+			System.out.println("No se encuentra la palabra");
+		}
 		
-		
-		
-		
-	}
+	}//main
 
-}
+}//class
