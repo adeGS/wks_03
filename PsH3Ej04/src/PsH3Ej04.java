533a47c70adaf527faa9b334eb7e3eb77babbb15
AdeGS <mssoperativesystem@gmail.com>
2017-11-22 21:12:26 +0100

    Final de clase 22/11/2017

--- PsH3Ej04/src/PsH3Ej04.java
+++ PsH3Ej04/src/PsH3Ej04.java
@@ -1,9 +1,15 @@
 /*4- Hacer un programa que imprima la suma de los 100 primeros números.*/
+
 public class PsH3Ej04 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int num, suma;
+		suma = 0;
+		for(num = 1; num <= 100; num++){
+			suma = suma + 1;
+		}
+		System.out.println("num+1: " + (num + 1) + " suma: " + suma);
 
-	}
+	}//main
 
-}
+}//class
